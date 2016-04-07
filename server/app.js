95ef68a2beb188a727a9afef847f01d42cbce105
Vitalii <v.hrytsiienko@gmail.com>
2016-04-08 00:27:50 +0300

    get tweets

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var config = require('./config');
+var twitter = require('twitter');
 
 var app = express.Router();
 
 app.get('/whoami', (req, res) => {
   res.send("You are a nac");
+});
+
+
+var client = twitter(config.twitter);
+var trackFilter = {track: 'javascript'};
+
+client.stream('statuses/filter', trackFilter, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet);
+  });
 });
 
 module.exports = app;
---
+++ server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter:{
+    consumer_key: '9SmwDwS04ZpTyxHiYepIvlm4i',
+    consumer_secret: '9iUTPrdyn8cDIhWgMkBPyO1CAITFizohXRIqREr9cJpyVPThar',
+    access_token_key: '4821215627-1acmpURRNVUb5KxMkVwjWQaJOEG4Opc0e3MIaPZ',
+    access_token_secret: 'V0Xl1QTwtfcv56JymzjCr4tJDBimOp3XsF1z9PMLMCYRs'
+  }
+};
